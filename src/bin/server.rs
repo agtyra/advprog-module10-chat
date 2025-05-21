a8d1d19554d0f5d543961f90bd232d257f44866f
agtyra <keziagtyra@gmail.com>
2025-05-21 15:46:48 +0700

    Experiment 2.2: Modifying port

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -19,6 +19,9 @@ async fn handle_connection(
             incoming = ws_receiver.next() => match incoming {
                 Some(Ok(msg)) => {
                     if let Some(text) = msg.as_text() {
+                        println!("From client {} \"{}\"", addr, text);
+
+                        // now build and send the broadcast banner
                         let banner = format!("{} says: {}", addr, text);
                         let _ = bcast_tx.send(banner);
                     } else if msg.is_close() {
@@ -54,7 +57,7 @@ async fn handle_connection(
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let (bcast_tx, _) = channel(16);
 
-    let listener = TcpListener::bind("127.0.0.1:2000").await?;
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("listening on port 2000");
 
     loop {
